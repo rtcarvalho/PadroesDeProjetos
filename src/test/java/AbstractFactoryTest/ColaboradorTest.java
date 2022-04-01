894b7f2182efe5b8d9a0bd2d4e375466350dd0e2
Raphael Torres <60902694+rtcarvalho@users.noreply.github.com>
2022-04-01 19:26:40 -0300

    Adição de novos padrões
    Esse é um padrão de projeto que permite que você produza famílias de objetos relacionados sem ter que especificar suas classes concretas.

---
+++ src/test/java/AbstractFactoryTest/ColaboradorTest.java
@@ -0,0 +1,24 @@
+package AbstractFactoryTest;
+
+import AbstractFactory.Colaborador;
+import AbstractFactory.FabricaAbstrat;
+import AbstractFactory.FabricaEstagiario;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColaboradorTest {
+
+    @Test
+    void deveRetornarContratoEstagiario() {
+        FabricaAbstrat fabrica = new FabricaEstagiario();
+        Colaborador colaborador = new Colaborador(fabrica);
+        assertEquals("contrato estagiario", colaborador.imprimirContrato());
+    }
+
+    @Test
+    void deveRetornarBeneficiosEstagiario() {
+        FabricaAbstrat fabrica = new FabricaEstagiario();
+        Colaborador colaborador = new Colaborador(fabrica);
+        assertEquals("Beneficio estagiario", colaborador.imprimiBeneficios());
+    }
